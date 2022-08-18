3a9627c90ed092d656dd1aa5a79044b449ac7248
TechieKeith <techiekeith@users.noreply.github.com>
2022-08-18 16:34:51 +0100

    run scripts

--- run_raspi1ap.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\qemu\qemu-system-arm.exe" -M raspi1ap -m 512M -serial stdio -usb -usbdevice keyboard -kernel dist\raspi1ap.elf
---
+++ scripts/run.ps1
@@ -0,0 +1,29 @@
+param (
+    [Parameter()][string]$machine = "raspi2b",
+    [Parameter()][string]$build = "release"
+)
+
+if ($machine -eq "raspi1ap") {
+    $memory = "512M"
+} else {
+    $memory = "1G"
+}
+if ($build -eq "release") {
+    $debug_opts = @()
+    $window_title = "${machine}"
+    $image = "dist\${machine}.elf"
+} else {
+    $debug_opts = @("-s", "-S")
+    $window_title = "${machine} GDB"
+    $image = "dist\${machine}_g.elf"
+}
+
+& "C:\Program Files\qemu\qemu-system-arm.exe" `
+    $debug_opts `
+    -M $machine `
+    -m $memory `
+    -name "$window_title" `
+    -serial stdio `
+    -usb `
+    -device usb-kbd `
+    -kernel $image
