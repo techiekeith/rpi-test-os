db67f081944ec43d7dc04a859d0ddb2bfd07fd97
Keith <811761+techiekeith@users.noreply.github.com>
2022-09-10 21:25:40 +0100

    Added display border (#22)

--- src/kernel/graphics.c
+++ src/kernel/graphics.c
@@ -4,6 +4,7 @@
 
 #include "../../include/common/stdbool.h"
 #include "../../include/common/stdint.h"
+#include "../../include/common/stdlib.h"
 #include "../../include/common/string.h"
 #include "../../include/kernel/charset.h"
 #include "../../include/kernel/framebuffer.h"
@@ -12,11 +13,13 @@
 #include "../../include/kernel/io.h"
 #include "../../include/kernel/mem_internal.h"
 #include "../../include/kernel/system_timer.h"
-#include "../../include/saa505x/glyphs.h"
 #include "../../include/raster/glyphs_8bit.h"
+#include "../../include/saa505x/glyphs.h"
 
 static int background_color = DEFAULT_BACKGROUND_COLOR;
 static int foreground_color = DEFAULT_FOREGROUND_COLOR;
+static int border_color = DEFAULT_BACKGROUND_COLOR;
+static int border_size = 0;
 static get_glyph_8bit_f get_glyph_8bit;
 static get_glyph_16bit_f get_glyph_16bit;
 static bool initialized;
@@ -35,7 +38,7 @@ void set_foreground_color(int color)
 void write_pixel(uint32_t x, uint32_t y, const uint32_t color)
 {
     if (!fbinfo.buf) return;
-    uint32_t location = y * fbinfo.pitch + x * fbinfo.bpp;
+    uint32_t location = (y + border_size) * fbinfo.pitch + (x + border_size) * fbinfo.bpp;
     if (location >= fbinfo.buf_size) return;
     uint32_t real_color = color;
     if (fbinfo.cursor_enabled && fbinfo.cursor_visible &&
@@ -55,7 +58,7 @@ void write_pixel(uint32_t x, uint32_t y, const uint32_t color)
 
 static void invert_pixel(uint32_t x, uint32_t y)
 {
-    uint32_t location = y * fbinfo.pitch + x * fbinfo.bpp;
+    uint32_t location = (y + border_size) * fbinfo.pitch + (x + border_size) * fbinfo.bpp;
     if (fbinfo.bpp == 1)
     {
         if (fbinfo.buf[location] >= 240)
@@ -194,9 +197,8 @@ static void move_cursor_forwards()
     }
 }
 
-static void clear_framebuffer_area(void *buffer, size_t size)
+static void clear_framebuffer_area(void *buffer, uint32_t color, size_t size)
 {
-    uint32_t color = colors[background_color];
     switch(fbinfo.depth)
     {
         case 8:
@@ -213,9 +215,28 @@ static void clear_framebuffer_area(void *buffer, size_t size)
     }
 }
 
+static void clear_border()
+{
+    if (border_size <= 0) return;
+    size_t border_height_bytes = fbinfo.pitch * border_size;
+    size_t border_width_bytes = fbinfo.bpp * border_size;
+    void *buffer = (void *) fbinfo.buf;
+    uint32_t color = colors[border_color];
+    clear_framebuffer_area(buffer, color, border_height_bytes);
+    buffer += border_height_bytes;
+    for (uint32_t i = border_size; i < fbinfo.height - border_size; i++)
+    {
+        clear_framebuffer_area(buffer, color, border_width_bytes);
+        clear_framebuffer_area(buffer + fbinfo.pitch - border_width_bytes, color, border_width_bytes);
+        buffer += fbinfo.pitch;
+    }
+    clear_framebuffer_area(buffer, color, border_height_bytes);
+}
+
 static void clear_framebuffer()
 {
-    clear_framebuffer_area((void *)fbinfo.buf, fbinfo.buf_size);
+    clear_framebuffer_area((void *)fbinfo.buf, colors[background_color], fbinfo.buf_size);
+    clear_border();
 }
 
 static void cursor_home()
@@ -224,28 +245,122 @@ static void cursor_home()
     fbinfo.current_row = 0;
 }
 
+static void move_or_copy_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, int dx, int dy, bool copy)
+{
+    if ((!dx && !dy) || !w || !h) return;
+    int src_start_x = x;
+    int src_start_y = y;
+    int dst_start_x = x + dx;
+    int dst_start_y = y + dy;
+    uint32_t rect_x = w;
+    uint32_t rect_y = h;
+
+    if (dst_start_x < 0)
+    {
+        rect_x += dst_start_x;
+        src_start_x -= dst_start_x;
+        dst_start_x = 0;
+    }
+    if (dst_start_y < 0)
+    {
+        rect_y += dst_start_y;
+        src_start_y -= dst_start_y;
+        dst_start_y = 0;
+    }
+    if (src_start_x + rect_x > fbinfo.width)
+    {
+        rect_x = fbinfo.width - src_start_x;
+    }
+    if (src_start_y + rect_y > fbinfo.height)
+    {
+        rect_y = fbinfo.height - src_start_y;
+    }
+    if (dst_start_x + rect_x > fbinfo.width)
+    {
+        rect_x = fbinfo.width - dst_start_x;
+    }
+    if (dst_start_y + rect_y > fbinfo.height)
+    {
+        rect_y = fbinfo.height - dst_start_y;
+    }
+    int row_size = fbinfo.bpp * rect_x;
+    int blank_size = fbinfo.bpp * dx;
+    void *src = (void *)fbinfo.buf + fbinfo.pitch * src_start_y + fbinfo.bpp * src_start_x;
+    void *dst = (void *)fbinfo.buf + fbinfo.pitch * dst_start_y + fbinfo.bpp * dst_start_x;
+    int increment = fbinfo.pitch;
+    if (dy > 0 || (dy == 0 && dx > 0))
+    {
+        src += fbinfo.pitch * (rect_y - 1);
+        dst += fbinfo.pitch * (rect_y - 1);
+        increment = -increment;
+    }
+    for (int i = rect_y; i; i--)
+    {
+        memmove(dst, src, row_size);
+        src += increment;
+        dst += increment;
+        if (!copy && dx != 0)
+        {
+            clear_framebuffer_area(src + ((dx > 0) ? 0 : (rect_x - abs(dx))), colors[background_color], blank_size);
+        }
+    }
+    if (!copy && dy != 0)
+    {
+        void *blank = (void *)fbinfo.buf + fbinfo.pitch * src_start_y + fbinfo.bpp * src_start_x;
+        if (dy < 0)
+        {
+            blank += fbinfo.pitch * (rect_y - abs(dy));
+        }
+        for (int i = abs(dy); i; i--)
+        {
+            clear_framebuffer_area(blank, colors[background_color], row_size);
+            blank += fbinfo.pitch;
+        }
+    }
+}
+
 static void scroll_down()
 {
-    uint32_t row_size = fbinfo.pitch * fbinfo.char_height;
-    size_t all_but_one_rows = fbinfo.buf_size - row_size;
-    memmove((void *)fbinfo.buf + row_size, (const void *)fbinfo.buf, all_but_one_rows);
-    clear_framebuffer_area((void *)fbinfo.buf, row_size);
+    if (!border_size) // Faster move for borderless display mode
+    {
+        uint32_t row_size = fbinfo.pitch * fbinfo.char_height;
+        size_t all_but_one_rows = fbinfo.buf_size - row_size;
+        memmove((void *)fbinfo.buf + row_size, (const void *)fbinfo.buf, all_but_one_rows);
+        clear_framebuffer_area((void *)fbinfo.buf, colors[background_color], row_size);
+        return;
+    }
+    move_or_copy_rect(border_size,
+                      border_size,
+                      fbinfo.width - (border_size * 2),
+                      fbinfo.height - (border_size * 2 + fbinfo.char_height),
+                      0,
+                      fbinfo.char_height,
+                      false);
 }
 
 static void scroll_up()
 {
-    uint32_t row_size = fbinfo.pitch * fbinfo.char_height;
-    size_t all_but_one_rows = fbinfo.buf_size - row_size;
-    memmove((void *)fbinfo.buf, (const void *)(fbinfo.buf + row_size), all_but_one_rows);
-    clear_framebuffer_area((void *)(fbinfo.buf + all_but_one_rows), row_size);
+    if (!border_size) // Faster move for borderless display mode
+    {
+        uint32_t row_size = fbinfo.pitch * fbinfo.char_height;
+        size_t all_but_one_rows = fbinfo.buf_size - row_size;
+        memmove((void *)fbinfo.buf, (const void *)(fbinfo.buf + row_size), all_but_one_rows);
+        clear_framebuffer_area((void *)(fbinfo.buf + all_but_one_rows), colors[background_color], row_size);
+        return;
+    }
+    move_or_copy_rect(border_size,
+                      border_size + fbinfo.char_height,
+                      fbinfo.width - (border_size * 2),
+                      fbinfo.height - (border_size * 2 + fbinfo.char_height),
+                      0,
+                      -fbinfo.char_height,
+                      false);
 }
 
 void graphics_putc(int c)
 {
     if (!initialized) return;
     bool cursor_enabled = disable_cursor();
-
-    int32_t num_rows = fbinfo.height / fbinfo.char_height;
 
     switch (c)
     {
@@ -286,7 +401,7 @@ void graphics_putc(int c)
         fbinfo.current_row++;
     }
 
-    if (fbinfo.current_row == num_rows)
+    if (fbinfo.current_row == fbinfo.rows)
     {
         scroll_up();
         fbinfo.current_row--;
@@ -325,10 +440,8 @@ static void set_default_display_mode()
 
 static void set_text_dimensions()
 {
-    fbinfo.columns = fbinfo.width / fbinfo.char_width;
-    fbinfo.rows = fbinfo.height / fbinfo.char_height;
-    fbinfo.current_column = 0;
-    fbinfo.current_row = 0;
+    fbinfo.columns = (fbinfo.width - border_size * 2) / fbinfo.char_width;
+    fbinfo.rows = (fbinfo.height - border_size * 2) / fbinfo.char_height;
 }
 
 static void set_cursor_rectangle(uint8_t top, uint8_t right, uint8_t bottom, uint8_t left)
@@ -337,6 +450,19 @@ static void set_cursor_rectangle(uint8_t top, uint8_t right, uint8_t bottom, uin
     fbinfo.cursor_right = right;
     fbinfo.cursor_bottom = bottom;
     fbinfo.cursor_left = left;
+}
+
+void set_border(int new_border_color, int new_border_size)
+{
+    border_color = new_border_color;
+    border_size = new_border_size;
+    clear_border();
+    set_text_dimensions();
+}
+
+int get_border_size()
+{
+    return border_size > 0 ? border_size : 0;
 }
 
 void set_8bit_charset(uint32_t width, uint32_t height, get_glyph_8bit_f get_glyph_function)
